b572f99f76aa4db03e7e5834058751b0a5bb5b46
Arnoud de Boer <a.deboer@geonovum.nl>
2018-01-05 11:01:58 +0100

    Update colofon.js

--- berichtenverkeer/media/colofon.js
+++ berichtenverkeer/media/colofon.js
@@ -14,10 +14,10 @@ var respecConfig = {
   ],
   shortName: "berichtenverkeer",
   pubDomain: "b5210574-ae81-49fc-a50b-a14b18a65cc0", // Aanpassen na verplaatsing naar nieuwe Github repos 
-  previousURI: "https://www.geonovum.nl/BRO",
+  //previousURI: "https://www.geonovum.nl/BRO",
   github: "https://github.com/adbgnm/b5210574-ae81-49fc-a50b-a14b18a65cc0",
-  previousPublishDate: "1900-01-01",
-  previousStatus: "GN-WV",
+  //previousPublishDate: "1900-01-01",
+  //previousStatus: "GN-WV",
   //localBiblio: {
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
