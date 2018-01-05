b691348c5510fbf92600d43a5336280001b3c932
Arnoud de Boer <a.deboer@geonovum.nl>
2018-01-05 10:28:34 +0100

    Initiele load o.b.v. Geo-BAG

---
+++ media/colofon.js
@@ -0,0 +1,30 @@
+var respecConfig = {
+  specStatus: "GN-DEF",
+  specType: "ST",
+  //canonicalURI: "WV",
+  publishDate: "2017-11-10",
+  editors: [
+    {
+      name: "Arnoud de Boer",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+      mailto: "a.deboer@geonovum.nl",
+      note: ""    
+    }  
+  ],
+  shortName: "berichtenverkeer",
+  pubDomain: "geobag", 
+  previousURI: "https://docs.geostandaarden.nl/geobag/Geo-BAG berichtenverkeer v1.0.2/",
+  github: "https://github.com/Geonovum/geobag",
+  previousPublishDate: "2017-08-01",
+  previousStatus: "GN-DEF",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Koppelvlakspecificatie Geo-BAG berichtenverkeer
+
+###Beschrijving koppelvlak tussen de registratiesystemen van Geo en BAG
+
+**Geonovum**
+
+versie: v1.0 
+
+datum: 2 december 2015
